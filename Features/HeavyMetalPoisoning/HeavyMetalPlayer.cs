eb905f8ae8c3ccebbd0d22d3b237f4cec6bfda6b
Smug B <43657791+SmugBlanco@users.noreply.github.com>
2023-11-10 18:06:21 -0500

    Create HeavyMetalPlayer.cs
    HeavyMetalPlayer controls the Heavy Metal Poisoning feature. That is, if a player stays near heavy metals for too long, then they'll get sicker and sicker.

---
+++ Features/HeavyMetalPoisoning/HeavyMetalPlayer.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+
+namespace WellsFargosDifficulty.Features.HeavyMetalPoisoning
+{
+    public class HeavyMetalPlayer : ModPlayer
+    {
+        /// <summary>
+        /// How much heavy metal is 'in' a player.
+        /// This is used to dynamically apply effects.
+        /// For reference, a concentration value of 60 means that the player has been near one tile for 60 ticks, or two tiles for 30 ticks, or three tiles for 20 ticks, etc.
+        /// </summary>
+        public int HeavyMetalConcentration { get; private set; }
+
+        /// <summary>
+        /// The amount of time since the player has last been near a heavy metal tile.
+        /// This is used to dynamically reduce <see cref="HeavyMetalConcentration"/>.
+        /// </summary>
+        public int HeavyMetalInteractionTimer { get; private set; }
+
+        /// <summary>
+        /// The value that <see cref="HeavyMetalInteractionTimer"/> needs to meet before <see cref="HeavyMetalConcentration"/> starts decreasing.
+        /// </summary>
+        public int HeavyMetalReductionThreshold { get; set; }
+
+        /// <summary>
+        /// An internally used timer to help dynamically reduce <see cref="HeavyMetalConcentration"/>
+        /// </summary>
+        public int HeavyMetalReductionTimer { get; private set; }
+
+        public override void ResetEffects() => HeavyMetalInteractionTimer++;
+
+        public override void PreUpdateMovement()
+        {
+            foreach (Point tilePosition in Player.TouchedTiles)
+            {
+                if (Main.IsTileSpelunkable(tilePosition.X, tilePosition.Y))
+                {
+                    HeavyMetalConcentration++;
+                    HeavyMetalInteractionTimer = 0;
+                }
+            }
+        }
+
+        public override void PostUpdateBuffs()
+        {
+            if (HeavyMetalInteractionTimer >= HeavyMetalReductionThreshold)
+            {
+                HeavyMetalReductionTimer++;
+                // Hard-coded to take an hour before 
+                int threshold = Math.Clamp(60 - (HeavyMetalInteractionTimer / HeavyMetalReductionThreshold), 1, 60);
+                if (HeavyMetalReductionTimer % threshold == 0)
+                {
+                    HeavyMetalConcentration--;
+                }
+            }
+        }
+
+        public override void SaveData(TagCompound tag)
+        {
+            if (HeavyMetalConcentration > 0)
+            {
+                tag[nameof(HeavyMetalConcentration)] = HeavyMetalConcentration;
+            }
+
+            if (HeavyMetalInteractionTimer > 0)
+            {
+                tag[nameof(HeavyMetalInteractionTimer)] = HeavyMetalInteractionTimer;
+            }
+        }
+
+        public override void LoadData(TagCompound tag)
+        {
+            HeavyMetalConcentration = tag.TryGet(nameof(HeavyMetalConcentration), out int value) ? value : 0;
+            HeavyMetalInteractionTimer = tag.TryGet(nameof(HeavyMetalInteractionTimer), out value) ? value : 0;
+        }
+    }
+}

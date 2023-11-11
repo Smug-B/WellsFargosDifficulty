01e2997aa52491758af068d17bcca9da68da568c
Smug B <43657791+SmugBlanco@users.noreply.github.com>
2023-11-10 22:19:25 -0500

    Applied Inflation to Services

---
+++ Features/AdaptiveEconomy/InflationItem.cs
@@ -0,0 +1,14 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace WellsFargosDifficulty.Features.AdaptiveEconomy
+{
+    public class InflationItem : GlobalItem
+    {
+        public override bool ReforgePrice(Item item, ref int reforgePrice, ref bool canApplyDiscount)
+        {
+            reforgePrice = (int)(reforgePrice * ModContent.GetInstance<InflationSystem>().TotalMultiplier);
+            return false;
+        }
+    }
+}
---
+++ Features/AdaptiveEconomy/InflationPlayer.cs
@@ -0,0 +1,11 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace WellsFargosDifficulty.Features.AdaptiveEconomy
+{
+    public class InflationPlayer : ModPlayer
+    {
+        public override void ModifyNursePrice(NPC nurse, int health, bool removeDebuffs, ref int price) 
+            => price = (int)(price * ModContent.GetInstance<InflationSystem>().TotalMultiplier);
+    }
+}

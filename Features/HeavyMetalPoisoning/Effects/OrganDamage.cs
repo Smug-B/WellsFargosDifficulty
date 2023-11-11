05b74c62e0e336a6d379edefe60bcf29ab199eb6
Smug B <43657791+SmugBlanco@users.noreply.github.com>
2023-11-10 19:00:50 -0500

    Create OrganDamage.cs
    General heavy metal poisoning effect, more to come?

---
+++ Features/HeavyMetalPoisoning/Effects/OrganDamage.cs
@@ -0,0 +1,20 @@
+﻿using Terraria;
+
+namespace WellsFargosDifficulty.Features.HeavyMetalPoisoning.Effects
+{
+    public class OrganDamage : HeavyMetalEffect
+    {
+        public override void BadLifeRegenEffects(Player player, int concentration)
+        {
+            player.lifeRegenTime = 0;
+
+            if (player.lifeRegen > 0)
+            {
+                player.lifeRegen = 0;
+            }
+
+            int reductionThreshold = player.GetModPlayer<HeavyMetalPlayer>().ReductionThreshold;
+            player.lifeRegen -= (concentration - reductionThreshold) / (reductionThreshold / 10);
+        }
+    }
+}

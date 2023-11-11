11fe8b0f0b3195d982ad4dfbcc5b5ac8ba51437e
Smug B <43657791+SmugBlanco@users.noreply.github.com>
2023-11-11 01:40:20 -0500

    Quicksand!
    Dangerous stuff...
    
    Standing still on sandblocks for over three seconds will cause the player to fall into the sand if said sand isn't 'structurally sound'.
    
    'Structural soundness' is measured by QuickSandThreshold: the minimum number of blocks of pure sand allowed before the surface sand isn't 'sound'.

---
+++ Features/Quicksand/QuicksandPlayer.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace WellsFargosDifficulty.Features.Quicksand
+{
+    public class QuicksandPlayer : ModPlayer
+    {
+        /// <summary>
+        /// Determines many layers of sand are required for the surface to be 'quicksand'.
+        /// </summary>
+        public const int QuickSandThreshold = 10;
+
+        /// <summary>
+        /// Whether or not the player is standing on 'quicksand'.
+        /// </summary>
+        public bool Quicksand { get; private set; }
+
+        public int LastMoved { get; private set; }
+
+        public int LastMovedThreshold { get; private set; } = 180;
+
+        public override void ResetEffects()
+        {
+            Quicksand = false;
+            LastMoved = Math.Clamp(LastMoved + 1, 0, LastMovedThreshold * 2);
+        }
+
+        public override void PostUpdateMiscEffects()
+        {
+            if (LastMoved < LastMovedThreshold)
+            {
+                return;
+            }
+
+            Point tilePositon = Player.Center.ToTileCoordinates();
+            int startX = tilePositon.X - 1;
+            int endX = tilePositon.X + 1 + (Player.Center.X % 16 < 8 ? -1 : 0);
+            int j = tilePositon.Y + 2;
+
+            for (int i = startX; i <= endX; i++)
+            {
+                if (!WorldGen.InWorld(i, j))
+                {
+                    continue;
+                }
+
+                Tile tile = Framing.GetTileSafely(i, j);
+                if (tile.HasTile && !Main.tileSand[tile.TileType])
+                {
+                    LastMoved = 0;
+                    return;
+                }
+
+                for (int searchY = 1; searchY < QuickSandThreshold; searchY++)
+                {
+                    if (!WorldGen.InWorld(i, j + searchY))
+                    {
+                        continue;
+                    }
+
+                    Tile searchTile = Framing.GetTileSafely(i, j + searchY);
+                    if (!searchTile.HasTile || !Main.tileSand[searchTile.TileType])
+                    {
+                        LastMoved = 0;
+                        return;
+                    }
+                }
+            }
+
+            Quicksand = true;
+            Player.position.Y++;
+            Player.controlDown = true;
+        }
+
+        public override void PostUpdateRunSpeeds()
+        {
+            if (Player.velocity.X != 0 || Player.velocity.Y < 0)
+            {
+                LastMoved -= 10;
+                return;
+            }
+
+            if (Quicksand)
+            {
+                Player.maxRunSpeed /= 10;
+                Player.runAcceleration /= 25;
+            }
+        }
+    }
+}

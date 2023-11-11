fa47af675759b19a3f4f7bb2cd89becca5939caf
Smug B <43657791+Smug-B@users.noreply.github.com>
2023-11-11 16:16:53 -0500

    Concussions
    A player receives a 'concussion' whenever they take more than 1/5 of their total hitpoints as damage.
    
    A concussed player's screen goes black before slowly returning to normal, they also get a confusion like effect.

---
+++ Features/ConcussiveImpact/ConcussionPlayer.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria.Graphics.Effects;
+using Terraria.Graphics.Shaders;
+using Terraria;
+using Terraria.ModLoader;
+using System;
+using Microsoft.Xna.Framework;
+using Terraria.ID;
+
+namespace WellsFargosDifficulty.Features.ConcussiveImpact
+{
+    public class ConcussionPlayer : ModPlayer
+    {
+        public static Effect ConcussionEffect { get; private set; }
+
+        public static Filter Concussion { get; private set; }
+
+        public float ConcussionProgress
+        {
+            get => Internal_ConcussionProgress;
+            set => Internal_ConcussionProgress = Math.Clamp(value, 0, 1);
+        }
+
+        private float Internal_ConcussionProgress = 1;
+
+        public override void Load()
+        {
+            if (Main.netMode == NetmodeID.Server)
+            {
+                return;
+            }
+
+            Ref<Effect> concussionEffect = new Ref<Effect>(ModContent.Request<Effect>("WellsFargosDifficulty/Features/ConcussiveImpact/Effects/Concussion", AssetRequestMode.ImmediateLoad).Value);
+            ConcussionEffect = concussionEffect.Value;
+            Filters.Scene["WellsFargosDifficulty:Concussion"] = new Filter(new ScreenShaderData(concussionEffect, "Concussion"), EffectPriority.VeryHigh);
+            Concussion = Filters.Scene["WellsFargosDifficulty:Concussion"];
+            Concussion.Load();
+        }
+
+        public override void PostUpdateMiscEffects()
+        {
+            if (Main.netMode == NetmodeID.Server)
+            {
+                return;
+            }
+
+            ConcussionProgress += 0.005f;
+            if (ConcussionProgress == 1)
+            {
+                if (Concussion.Active)
+                {
+                    Filters.Scene.Deactivate("WellsFargosDifficulty:Concussion");
+                    ConcussionEffect.Parameters["active"].SetValue(false);
+                }
+                return;
+            }
+
+            if (!Concussion.Active)
+            {
+                Filters.Scene.Activate("WellsFargosDifficulty:Concussion");
+            }
+
+            ConcussionEffect.Parameters["active"].SetValue(true);
+            ConcussionEffect.Parameters["concussiveShade"].SetValue(new Vector4(0));
+            ConcussionEffect.Parameters["concussiveProgress"].SetValue(1 - (float)Math.Pow(ConcussionProgress, 2));
+            ConcussionEffect.Parameters["dazeProgress"].SetValue((1 - ConcussionProgress) / 2);
+            ConcussionEffect.Parameters["dazeOffset"].SetValue(new Vector2((float)Math.Sin(ConcussionProgress * 20), (float)Math.Cos(ConcussionProgress * 20)) * (1 - ConcussionProgress) / 20);
+        }
+
+        public override void PostHurt(Player.HurtInfo info)
+        {
+            if (info.Damage >= Player.statLifeMax2 / 5)
+            {
+                ConcussionProgress = 0;
+            }
+        }
+    }
+}
---
+++ Features/ConcussiveImpact/Effects/Concussion.fx
@@ -0,0 +1,54 @@
+﻿sampler uImage0 : register(s0);
+sampler uImage1 : register(s1);
+sampler uImage2 : register(s2);
+sampler uImage3 : register(s3);
+float3 uColor;
+float uOpacity;
+float3 uSecondaryColor;
+float uTime;
+float2 uScreenResolution;
+float2 uScreenPosition;
+float2 uTargetPosition;
+float2 uImageOffset;
+float uIntensity;
+float uProgress;
+float2 uDirection;
+float2 uZoom;
+float2 uImageSize1;
+float2 uImageSize2;
+float2 uImageSize3;
+
+bool active;
+float4 concussiveShade;
+float concussiveProgress;
+float dazeProgress;
+float2 dazeOffset;
+
+float4 Tint(float4 oldColor, float4 refColor, float tintProgress)
+{
+    float4 colorDifference = (refColor - oldColor) * tintProgress;
+    return oldColor + colorDifference;
+}
+
+float4 Concussion(float4 position : SV_POSITION, float2 coords : TEXCOORD0) : COLOR0
+{
+    if (!active)
+    {
+        return tex2D(uImage0, coords);
+    }
+        
+    float4 output = Tint(tex2D(uImage0, coords), tex2D(uImage0, coords + dazeOffset), dazeProgress);
+    output = Tint(output, tex2D(uImage0, coords + float2(-dazeOffset.x, dazeOffset.y)), dazeProgress);
+    output = Tint(output, tex2D(uImage0, coords + float2(dazeOffset.y, dazeOffset.x)), dazeProgress);
+    output = Tint(output, tex2D(uImage0, coords + float2(-dazeOffset.y, dazeOffset.x)), dazeProgress);
+    output = Tint(output, concussiveShade, concussiveProgress);
+    return output;
+}
+
+technique Technique1
+{
+    pass Concussion
+    {
+        PixelShader = compile ps_2_0 Concussion();
+    }
+}

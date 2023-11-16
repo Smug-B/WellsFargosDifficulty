4fad9e14fb1b4003f58a5e2f125dc47b17050f5c
Smug B <43657791+Smug-B@users.noreply.github.com>
2023-11-15 21:54:04 -0500

    Icarus' Folly
    Removed inherent fall damage removal from equipping wings.
    
    Players will need to at least glide to avoid fall damage; their carelessness will lead to their downfall.

---
+++ Features/Icarus/IcarusSystem.cs
@@ -0,0 +1,34 @@
+﻿using log4net.Repository.Hierarchy;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using System;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace WellsFargosDifficulty.Features.Icarus
+{
+    public class IcarusSystem : ModPlayer
+    {
+        public override void Load() => IL_Player.Update += ApplyIcarusFolly;
+
+        private void ApplyIcarusFolly(ILContext il)
+        {
+            ILCursor cursor = new ILCursor(il);
+            if (!cursor.TryGotoNext(MoveType.After, i => i.MatchStloc(57)))
+            {
+                return;
+            }
+
+            cursor.Emit(OpCodes.Ldc_I4_0);
+            cursor.Emit(OpCodes.Stloc, 57);
+        }
+
+        public override void PreUpdateMovement()
+        {
+            if (Player.velocity.Y <= 0f)
+            {
+                Player.fallStart = (int)(Player.position.Y / 16f);
+            }
+        }
+    }
+}
